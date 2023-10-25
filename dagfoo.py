c0b019e961f8d51e59f0507de1d6d94089618b2d
James Caple <capable2@gmail.com>
2023-10-25 17:55:38 -0400

    initial commit

---
+++ dagfoo.py
@@ -0,0 +1,61 @@
+#
+# Description:
+#
+# Given a Directed Acyclic Graph, calculate the longest directed path from 
+# that vertex.
+#
+# Resources:
+# - https://networkx.org/documentation/networkx-1.9.1/index.html
+# - https://stackoverflow.com/questions/39686213/networkx-most-efficient-way-to-find-the-longest-path-in-a-dag-at-start-vertex-wi
+#
+# Filename: dagfoo.py
+#
+# Requirements:
+# - pip install networkx
+#
+# Optional Libraries:
+# - pip install matplotlib
+#
+
+import networkx as nx
+# import matplotlib.pyplot as plt
+
+G = nx.DiGraph()
+G.add_edges_from(
+    [
+        (1,2), 
+        (1,3),
+        (2,4),
+        (3,4),
+        (4,5),
+        (5,6),
+        (2,7),
+        (7,8),
+        (7,9),
+        (7,10),
+        (6,1)
+    ]
+)
+
+# Display Graph for visual inspection if you want
+# nx.draw(G, with_labels=True, font_weight='bold')
+# plt.show()
+
+# The Vertex to start at
+startNode = 1
+
+# The Vertex to end at
+endNode = 6
+
+longestRoute = 0
+routes = []
+for path in nx.all_simple_paths(G, source=startNode, target=endNode): 
+    if (len(path) > longestRoute):
+        longestRoute = len(path)
+        routes = []
+        routes.append(path)
+    elif (len(path) == longestRoute):
+        routes.append(path)
+
+print("Longest Route -> ", longestRoute)
+print(routes)
